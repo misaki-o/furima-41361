98f841fd8f12389ed711063448e749bb222e0e4d
misaki-o <choco5st@gmail.com>
2024-08-24 19:06:48 +0900

    出品機能(javascript実装以外)

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,17 +1,23 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   #下記のActive_hashモデルに紐づく
-  belongs_to_active_hash :category, :condition, :prefecture, :shipping_day, :shipping_fee
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :shipping_day
+  belongs_to_active_hash :shipping_fee
   
   #バリデーション
+  validates :image, presence: true
   validates :item_name, presence: true
   validates :description, presence: true
-  validates :category_id, numericality: { other_than: 1} , presence: true
-  validates :condition_id, numericality: { other_than: 1} , presence: true
-  validates :prefecture_id, numericality: { other_than: 1} , presence: true
-  validates :shipping_day_id, numericality: { other_than: 1} , presence: true
-  validates :shipping_fee_id, numericality: { other_than: 1} , presence: true
-  validates :price, presence: true
+  validates :category_id, presence: true
+  validates :condition_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :shipping_day_id, presence: true
+  validates :shipping_fee_id, presence: true
+  validates :price, presence: true, format: { with: /\A[0-9]+\z/ },
+  numericality: {greater_than_or_equal_to:300, less_than_or_equal_to:9_999_999 }
   validates :user_id, presence: true
 
   #アソシエーション
