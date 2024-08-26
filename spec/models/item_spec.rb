8b9f62021ac80fa2a87073156d95ecae2a81d921
misaki-o <choco5st@gmail.com>
2024-08-26 17:50:10 +0900

    単体テストコード（異常系追加）

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Item, type: :model do
   before do
-    @user = FactoryBot.create(:user)
     @item = FactoryBot.build(:item)
   end
 
@@ -58,6 +57,26 @@ RSpec.describe Item, type: :model do
       @item.price = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Price can't be blank")
+     end
+     it "販売価格が300未満は登録できない" do
+      @item.price = '299'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+     end
+     it "販売価格が9_999_999を超えると登録できない" do
+      @item.price = '100000000000'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
+     end
+     it "販売価格が半角数字以外だと登録できない" do
+      @item.price = '100あああ'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+     end
+     it "userがひもづいてないと登録できない" do
+      @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("User must exist")
      end
     end
   end
